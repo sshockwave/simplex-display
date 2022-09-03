53ef76c9eeec15c63a26e8deb482b768a21e5e51
Kerry Su <me@sshockwave.net>
2022-09-03 13:26:28 +0800

    Refactor(table) support hovering on all areas

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -3,85 +3,97 @@ import { useState } from 'react';
 
 export function SimplexTable({ table, hl }) {
   const var_list = Array.from(Object.values(table.var_to_id));
+  const nvars = var_list.length;
+  const nrows = table.rows.length;
   hl = hl || [null, null];
-  const [focus, set_focus] = useState(hl);
+  const [c_row, set_row] = useState(hl[0]);
+  const [c_col, set_col] = useState(hl[1]);
+  function f_row(i) {
+    return () => set_row(i);
+  }
+  function f_col(j) {
+    return () => set_col(j);
+  }
   return <table
     className='table text-center simplex-table'
-    onMouseLeave={() => set_focus(hl)}
+    onMouseLeave={() => {
+      set_row(hl[0]);
+      set_col(hl[1]);
+    }}
   >
     <tbody>
-      <tr>
-        <th colSpan='3'>
+      <tr onMouseOver={f_row(0)}>
+        <th colSpan='3' onMouseOver={f_col(0)}>
           <Equation>{'c_j'}</Equation>
         </th>
         {var_list.map((var_id, idx) => (
-          <td key={idx} className={focus[1] === idx ? 'active' : ''}>
+          <td key={idx} className={c_col === idx ? 'active' : ''} onMouseOver={f_col(idx)}>
             <Equation>{table.original_target_coef[var_id].to_katex()}</Equation>
           </td>
         ))}
-        <th rowSpan='2' className='align-middle'>
-          <Equation>{`\\beta_{i${focus[1] === null ? '' : focus[1]}}`}</Equation>
+        <th rowSpan='2' className='align-middle' onMouseOver={f_col(nvars - 1)}>
+          <Equation>{`\\beta_{i${c_col === null ? '*' : c_col}}`}</Equation>
         </th>
       </tr>
-      <tr>
-        <th><Equation>{'c_B'}</Equation></th>
-        <th><Equation>{'x_B'}</Equation></th>
-        <th><Equation>{'P_0'}</Equation></th>
+      <tr onMouseOver={f_row(0)}>
+        <th onMouseOver={f_col(0)}><Equation>{'c_B'}</Equation></th>
+        <th onMouseOver={f_col(0)}><Equation>{'x_B'}</Equation></th>
+        <th onMouseOver={f_col(0)}><Equation>{'P_0'}</Equation></th>
         {var_list.map((var_id, idx) => (
-          <td key={idx} className={focus[1] === idx ? 'active' : ''}>
+          <td key={idx} className={c_col === idx ? 'active' : ''} onMouseOver={f_col(idx)}>
             <Equation>{var_to_math(table.id_to_var[var_id])}</Equation>
           </td>
         ))}
       </tr>
-      {table.rows.map((row, idx) => <tr key={idx} className={idx === focus[0] ? 'active' : ''}>
-        <td>
+      {table.rows.map((row, idx) => <tr key={idx} className={idx === c_row ? 'active' : ''} onMouseOver={f_row(idx)}>
+        <td onMouseOver={f_col(0)}>
           <Equation>
             {table.original_target_coef[row.base_id].to_katex()}
           </Equation>
         </td>
-        <td>
+        <td onMouseOver={f_col(0)}>
           <Equation>
             {var_to_math(table.id_to_var[row.base_id])}
           </Equation>
         </td>
-        <td>
+        <td onMouseOver={f_col(0)}>
           <Equation>{row.p0.to_katex()}</Equation>
         </td>
-        {var_list.map((var_id, idx2) => {
+        {var_list.map((var_id, idx) => {
           const v = row.coef[var_id];
           return <td
-            className={idx2 === focus[1] ? 'active' : ''}
-            key={idx2}
-            onMouseOver={() => set_focus([idx, idx2])}
+            className={idx === c_col ? 'active' : ''}
+            key={idx}
+            onMouseOver={f_col(idx)}
           >
             {v.is_zero() ? null : <Equation>{v.to_katex()}</Equation>}
           </td>;
         })}
-        <td><Equation>{(() => {
-          if (focus[0] === null) {
+        <td onMouseOver={f_col(nvars - 1)}><Equation>{(() => {
+          if (c_col === null) {
             return '\\text{---}';
           }
-          const v = row.coef[var_list[focus[1]]];
+          const v = row.coef[var_list[c_col]];
           if (v.is_zero() || v.is_neg()) {
             return '\\infty';
           }
           return row.p0.div(v).to_katex();
         })()}</Equation></td>
       </tr>)}
-      <tr>
-        <th colSpan='2'>
+      <tr onMouseOver={f_row(nrows - 1)}>
+        <th colSpan='2' onMouseOver={f_col(0)}>
           <Equation>{'-Z'}</Equation>
         </th>
-        <td>
+        <td onMouseOver={f_col(0)}>
           <Equation>{table.target_p0.neg().to_katex()}</Equation>
         </td>
         {var_list.map((var_id, idx) => {
           const v = table.target_coef[var_id];
-          return <td key={idx} className={idx === focus[1] ? 'active' : ''}>
+          return <td key={idx} className={idx === c_col ? 'active' : ''} onMouseOver={f_col(idx)}>
             <Equation>{v.to_katex()}</Equation>
           </td>;
         })}
-        <td></td>
+        <td onMouseOver={f_col(nvars - 1)}></td>
       </tr>
     </tbody>
   </table>;
