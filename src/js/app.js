6a4eb44cd7703e309caf8db6525f687fe8b1145b
Kerry Su <me@sshockwave.net>
2022-10-28 21:57:29 +0800

    Update input

--- src/js/app.js
+++ src/js/app.js
@@ -32,35 +32,35 @@ function gen_mock_table() {
 
 function gen_displayable_table() {
   let table = new Table;
-  table.var_to_id = { 'x1': 0, 'x2': 1 };
-  table.id_to_var = ['x1', 'x2'];
+  table.var_to_id = { 'x1': 0, 'x2': 1, 'x3': 2 };
+  table.id_to_var = ['x1', 'x2', 'x3'];
   table.rows = [
     {
-      coef: [Fraction.from_num(2), Fraction.from_num(2)],
+      coef: [Fraction.from_num(-2), Fraction.from_num(1), Fraction.from_num(-3)],
       rel: '\\le',
-      p0: Fraction.from_num(12),
+      p0: Fraction.from_num(-4),
       base_id: -1,
     },
     {
-      coef: [Fraction.from_num(1), Fraction.from_num(2)],
-      rel: '\\le',
-      p0: Fraction.from_num(8),
+      coef: [Fraction.from_num(1), Fraction.from_num(-2), Fraction.from_num(4)],
+      rel: '\\ge',
+      p0: Fraction.from_num(-6),
       base_id: -1,
     },
     {
-      coef: [Fraction.from_num(4), Fraction.from_num(0)],
-      rel: '\\le',
-      p0: Fraction.from_num(16),
-      base_id: -1,
-    },
-    {
-      coef: [Fraction.from_num(0), Fraction.from_num(4)],
-      rel: '\\le',
-      p0: Fraction.from_num(12),
+      coef: [Fraction.from_num(2), Fraction.from_num(1), Fraction.from_num(-2)],
+      rel: '\\ge',
+      p0: Fraction.from_num(2),
       base_id: -1,
     },
   ];
-  table.target_coef = [Fraction.from_num(2), Fraction.from_num(3)];
+  table.target_coef = [Fraction.from_num(3), Fraction.from_num(1), Fraction.from_num(-2)];
+  table.var_non_std = [
+    { id: 0, rel: '\\ge', val: Fraction.from_num(2) },
+    { id: 1, rel: 'any', },
+    { id: 2, rel: '\\le', val: Fraction.from_num(0) },
+  ];
+  table.target_is_max = false;
   return table;
 }
 
