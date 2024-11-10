5ac6562672c506345eff534b87d50db72e01014c
Kerry Su <me@sshockwave.net>
2024-11-10 23:01:27 +0800

    Feat compress data, not working due to async

--- src/js/utils.js
+++ src/js/utils.js
@@ -12,6 +12,51 @@ export function clone(obj) {
   return Object.assign({}, obj);
 }
 
+async function compressData(str) {
+  const textEncoder = new TextEncoder();
+  const uint8Array = textEncoder.encode(str);
+
+  const cs = new CompressionStream('gzip');
+  const compressedStream = new ReadableStream({
+    async start(controller) {
+      controller.enqueue(uint8Array);
+      controller.close();
+    }
+  }).pipeThrough(cs);
+
+  const reader = compressedStream.getReader();
+  const chunks = [];
+  while (({ done, value } = await reader.read()) && !done) {
+    chunks.push(String.fromCharCode(...new Uint8Array(value)));
+  }
+  return btoa(chunks.join(''));
+}
+
+async function decompressData(str) {
+  const textDecoder = new TextDecoder();
+  const uint8Array = new Uint8Array(atob(str).split('').map(c => c.charCodeAt(0)));
+  const cs = new DecompressionStream('gzip');
+  const decompressedStream = new ReadableStream({
+    async start(controller) {
+      controller.enqueue(uint8Array);
+      controller.close();
+    }
+  }).pipeThrough(cs);
+
+  const reader = decompressedStream.getReader();
+  const chunks = [];
+  while (({ done, value } = await reader.read()) && !done) {
+    chunks.push(value);
+  }
+  const joined_chunks = new Uint8Array(chunks.reduce((acc, chunk) => acc + chunk.length, 0));
+  let offset = 0;
+  for (const chunk of chunks) {
+    joined_chunks.set(chunk, offset);
+    offset += chunk.length;
+  }
+  return textDecoder.decode(joined_chunks);
+}
+
 export function useStateParams(initialState, paramsName, serialize, deserialize) {
   const navigate = useNavigate();
   const location = useLocation();
